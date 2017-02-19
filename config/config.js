4d1cf833d661db920f714afcd0ea51cab28010be
Ben Beltran <ben@nsovocal.com>
2017-02-18 21:45:32 -0600

    Add redis related configs

--- config/config.js
+++ config/config.js
@@ -12,7 +12,22 @@ const internals = {};
  * @memberof DeadDrop
  * @typedef {object} tConfiguration
  * @property {number} [port=1988] the port where the app will listen on
+ * @property {DeadDrop.tRedisConfiguration} redis the configuration to
+ * connect to the redis server
  */
 module.exports = internals.Config = {
   port: Getenv.int('DEAD_DROP_PORT', 1988)
+
+  /**
+   * Information required to connect to the redis server
+   *
+   * @memberof DeadDrop
+   * @typedef {object} tRedisConfiguration
+   * @property {string} host the location of the redis host
+   * @property {string} [post=6379] port where redis server is listening
+   */
+  redis: {
+    host: Getenv('DEAD_DROP_REDIS_HOST'),
+    port: Getenv.int('DEAD_DROP_REDIS_PORT', 6379)
+  },
 };
